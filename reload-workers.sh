1183ca9f0dba9e6df9b21c7c3cef891a822d0314
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2015-07-02 17:48:38 +0300

    Make reload-worker-list more reliable.

--- reload-workers.sh
+++ reload-workers.sh
@@ -5,13 +5,15 @@ set -e
 # and have "citus" in their name
 cat /etc/hosts | \
     grep "citus" | \
+    # Remove ip and junk info attached by docker
+    cut -f2 | cut -f1 -d' ' | \
     # :TRICKY: order citus hosts alphabetically, in hopes similar entries are
     # grouped together. This makes the hostnames in worker list predictable.
-    sort -k 1,1 -k 2,2 | sort -k 1,1 -u | \
-    awk -v N=2 '{print $N}' | \
-    xargs -I host echo "host 5432" > "$PGDATA/pg_worker_list.conf"
+    sort | \
+    uniq | \
+    xargs -I host echo -e "host\t5432" >> "$PGDATA/pg_worker_list.conf"
 
 if [ "$CITUS_STANDALONE" ]
 then
-  echo "localhost 5432" >> "$PGDATA/pg_worker_list.conf"
+  echo -e "localhost\t5432" >> "$PGDATA/pg_worker_list.conf"
 fi
